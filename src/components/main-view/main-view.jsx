e8dcc5c4311a578b37b7dadf0d7d1fdb512790e1
Kari <kari.lynn.dreyer@gmail.com>
2024-07-08 16:37:29 +0200

    fix: remove unneeded info from array

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -5,7 +5,6 @@ import { MovieView } from "../movie-view/movie-view";
 export const MainView = () => {
     const [movies, setMovies] = useState([
         {
-            _id: ObjectID('665ef9509dddec05c68fcd0c'),
             title: "Zero Dark Thirty",
             description: "A chronicle of the decade-long hunt for al-Qaeda terrorist leader Osama bin Laden after the September 2001 attacks, and his death at the hands of the Navy SEAL Team 6 in May 2011.",
             genre: "Action",
@@ -14,7 +13,6 @@ export const MainView = () => {
             featured: false
         },
         {
-            _id: ObjectID('665efb609dddec05c68fcd13'),
             title: "Marie Antoinette",
             description: "The retelling of France's iconic but ill-fated queen, Marie Antoinette. From her betrothal and marriage to Louis XVI at 15 to her reign as queen at 19 and ultimately the fall of Versailles.",
             genre: "Historical",
@@ -23,7 +21,6 @@ export const MainView = () => {
             featured: false
         },
         {
-            _id: ObjectID('665efb3b9dddec05c68fcd11'),
             title: "A Wrinkle in Time",
             description: "After the disappearance of her scientist father, three peculiar beings send Meg, her brother, and her friend to space in order to find him.",
             genre: "Fantasy",
