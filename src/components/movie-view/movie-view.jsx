e294ea60bb6b6da5e6c8ed132d53a3d963edb7b9
Kari <kari.lynn.dreyer@gmail.com>
2024-07-23 11:37:06 +0200

    Fix: update keys for movie object

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,27 +1,49 @@
+import PropTypes from 'prop-types';
+
 export const MovieView = ({ movie, onBackClick }) => {
     return (
         <div>
             <div>
-                <img src={movie.ImagePath} />
+                <img src={movie.imagePath} />
             </div>
             <div>
                 <span>Title: </span>
-                <span>{movie.Title}</span>
+                <span>{movie.title}</span>
             </div>
             <div>
                 <span>Director: </span>
-                <span>{movie.Director}</span>
+                <span>{movie.director.name}</span>
             </div>
             <div>
                 <span>Description: </span>
-                <span>{movie.Description}</span>
+                <span>{movie.description}</span>
             </div>
             <div>
                 <span>Genre: </span>
-                <span>{movie.Genre}</span>
+                <span>{movie.genre.name}</span>
             </div>
             <button onClick={onBackClick}>Back</button>
         </div>
 
-    )
-}
+    );
+};
+
+// Define Prop Types for Movie View
+MovieView.propTypes = {
+    movie: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+        imagePath: PropTypes.string.isRequired,
+        genre: PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            description: PropTypes.string
+        }).isRequired,
+        director: PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            bio: PropTypes.string,
+            birth: PropTypes.string,
+            death: PropTypes.string
+        }).isRequired
+    }).isRequired,
+    onBackClick: PropTypes.func.isRequired
+};
