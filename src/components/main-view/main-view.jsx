9578a0bc8a9dcde822ec5e77cd59c28e93f488c4
Kari <kari.lynn.dreyer@gmail.com>
2024-07-23 12:35:21 +0200

    Add LoginView component to MainView

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,11 +2,12 @@ import { useState, useEffect } from "react";
 import PropTypes from 'prop-types';
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
 
 export const MainView = () => {
     const [movies, setMovies] = useState([]);
-
     const [selectedMovie, setSelectedMovie] = useState(null);
+    const [user, setUser] = useState(null);
 
     useEffect(() => {
         fetch("https://movie-nest-app-630a7e8ce836.herokuapp.com/movies")
@@ -35,6 +36,10 @@ export const MainView = () => {
             setMovies(moviesFromApi);
           });
       }, []);
+
+    if (!user) {
+        return <LoginView />;
+    }
 
     if (selectedMovie) {
         return (
