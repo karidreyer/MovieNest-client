08cfdd853c4ef8a317a3e6a1bfa8b97c978cc8d4
Kari <kari.lynn.dreyer@gmail.com>
2024-07-25 10:08:02 +0200

    Add login functionality

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,13 +1,58 @@
-export const LoginView = () => {
+import { useState } from "react";
+
+export const LoginView = ({ onLoggedIn }) => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (event) => {
+        // This prevents the default behaviour of the form (to reload the entire page)
+        event.preventDefault();
+
+        const data = {
+            Username: username,
+            Password: password
+        };
+
+        fetch("https://movie-nest-app-630a7e8ce836.herokuapp.com/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log("Login response: ", data);
+                if (data.user) {
+                    onLoggedIn(data.user, data.token);
+                } else {
+                    alert("No such user");
+                }
+            })
+            .catch((e) => {
+                alert("Something went wrong");
+            });
+    };
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>
                 Username: 
-                <input type="text"/>
+                <input 
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                />
             </label>
             <label>
                 Password:
-                <input type="password"/>
+                <input 
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
             </label>
             <button type="submit">Submit</button>
         </form>
