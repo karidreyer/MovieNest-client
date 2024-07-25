37180e28e13c66da63068805317e0dd6ab17d6a0
Kari <kari.lynn.dreyer@gmail.com>
2024-07-25 11:51:39 +0200

    Add SignupView component to MainView

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
     const storedUser = JSON.parse(localStorage.getItem("user"));
@@ -49,12 +50,16 @@ export const MainView = () => {
 
     if (!user) {
         return (
-            <LoginView 
-                onLoggedIn={(user, token) => {
-                    setUser(user);
-                    setToken(token);
-                }}
-            />
+            <>
+                <LoginView 
+                    onLoggedIn={(user, token) => {
+                        setUser(user);
+                        setToken(token);
+                    }}
+                />
+                or
+                <SignupView />
+            </>
         );
     }
 
