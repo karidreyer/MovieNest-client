ae415d9fb1c7ff64b4f219aee18df7a12aeac7ad
Kari <kari.lynn.dreyer@gmail.com>
2024-07-31 12:41:51 +0200

    Add Container for React Bootstrap UI updates

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,21 @@
 import { createRoot } from "react-dom/client"
 import { MainView } from "./components/main-view/main-view";
+import Container from 'react-bootstrap/Container';
+
+
+// Import React Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
 
 //Import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
 
 //Main component (will eventually use all the others)
 const MovieNestApplication = () => {
-    return <MainView />;
+    return (
+        <Container style={{border: "1px solid red"}}>
+            <MainView />
+        </Container>
+    );
 };
 
 //Finds the root of the app
