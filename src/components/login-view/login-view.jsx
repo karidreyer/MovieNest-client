4013d8664d8b9e1ecdfff764dd939a38cc93f407
Kari <kari.lynn.dreyer@gmail.com>
2024-08-06 11:49:08 +0200

    Update LoginView UI using React Bootstrap

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import PropTypes from 'prop-types';
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
+import { Button, Col, Container, FloatingLabel, Form, Nav, Row } from "react-bootstrap";
+import './login-view.scss';
 
 export const LoginView = ({ onLoggedIn }) => {
     const [username, setUsername] = useState("");
@@ -40,30 +40,45 @@ export const LoginView = ({ onLoggedIn }) => {
     };
 
     return (
-        <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="formUsername">
-            <Form.Label>Username: </Form.Label>
-                <Form.Control 
-                    type="text"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    required
-                    minLength="3"
-                />
-            </Form.Group>
-            
-            <Form.Group>
-                <Form.Label>Password:</Form.Label>
-                    <Form.Control 
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                    />
-            </Form.Group>
-            
-            <Button variant="primary" type="submit">Submit</Button>
-        </Form>
+        <Container className="d-flex flex-column align-items-center justify-content-center vh-100">
+            <Row className="text-left">
+                <Col>
+                    <p className="mb-1">Welcome to</p>
+                    <h1>Movie Nest</h1>
+                </Col>
+            </Row>
+            <Row className="w-100 justify-content-center">
+                <Col md={6} lg={4}>
+                    <Form onSubmit={handleSubmit}>
+                        <FloatingLabel controlId="floatingInput" label="Username" className="mb-3">
+                            <Form.Control 
+                                type="text"
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                                required
+                                minLength="3"
+                            />
+                        </FloatingLabel>
+                        
+                        <FloatingLabel controlId="floatingPassword" label="Password" className="mb-3">
+                                <Form.Control 
+                                    type="password"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    required
+                                />
+                        </FloatingLabel>
+                        
+                        <div className="d-flex justify-content-end mb-3">
+                            <Button variant="primary" type="submit">Login</Button>
+                        </div>
+                    </Form>
+                    <Col className="text-end">
+                        <Nav.Link to="/signup" className="p-0">Don't have an account?</Nav.Link>
+                    </Col>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
