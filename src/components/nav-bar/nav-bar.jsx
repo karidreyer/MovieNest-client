e08eb0b5ba327d2db6b10f97057f76f59bc50346
Kari <kari.lynn.dreyer@gmail.com>
2024-08-06 12:03:20 +0200

    Create NavBar component

--- src/components/nav-bar/nav-bar.jsx
+++ src/components/nav-bar/nav-bar.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
-import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Navbar, Container, Nav, Button } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 
 export const NavBar = ({ onLogout }) => {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
-            <Container>
-                <Navbar.Brand href="#home">Movie Nest</Navbar.Brand>
+            <Container fluid>
+                {/* LINKS  NEED UPDATING HERE */}
+                <Navbar.Brand href="http://localhost:1234/">myFlix</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="http://localhost:1234/">Home</Nav.Link>
                         <Nav.Link href="#profile">Profile</Nav.Link>
                     </Nav>
                     <Nav>
-                        <button onClick={onLogout}>Logout</button>
+                        <Button variant="link" onClick={onLogout}>Logout</Button>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
