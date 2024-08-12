bbe06d34bea86ce4be509b8880a5f96f31d7156c
Kari <kari.lynn.dreyer@gmail.com>
2024-08-12 11:53:49 +0200

    Update routing for Navigation Bar

--- src/components/nav-bar/nav-bar.jsx
+++ src/components/nav-bar/nav-bar.jsx
@@ -1,21 +1,41 @@
-import React from 'react';
-import { Navbar, Container, Nav, Button } from 'react-bootstrap';
-import PropTypes from 'prop-types';
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-export const NavBar = ({ onLogout }) => {
+export const NavBar = ({ user, onLoggedOut }) => {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container fluid>
-                {/* LINKS  NEED UPDATING HERE */}
-                <Navbar.Brand href="http://localhost:1234/">myFlix</Navbar.Brand>
+                <Navbar.Brand as={Link} to="/">
+                    movieNest
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="http://localhost:1234/">Home</Nav.Link>
-                        <Nav.Link href="#profile">Profile</Nav.Link>
-                    </Nav>
-                    <Nav>
-                        <Button variant="link" onClick={onLogout}>Logout</Button>
+                        {!user && (
+                            <>
+                                <Nav.Link as={Link} to="/login">
+                                    Login
+                                </Nav.Link>
+                                <Nav.Link as={Link} to="/signup">
+                                    Signup
+                                </Nav.Link>
+                            </>
+                        )}
+                        {user && (
+                            <>
+                                <Nav.Link as={Link} to="/">
+                                    Home
+                                </Nav.Link>
+                                <Nav.Link as={Link} to="/profile">
+                                    Profile
+                                </Nav.Link>
+                                <Nav.Link onClick={onLoggedOut}>
+                                    Logout
+                                </Nav.Link>
+                            </>
+                        )}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
